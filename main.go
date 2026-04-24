a96992e95641d5791046c9a40bf35847f109ea1b
dachiko1233 <dachimari9@email.com>
2026-04-24 18:12:18 +0300

    env

--- main.go
+++ main.go
@@ -3,18 +3,23 @@ package main
 import (
 	"goapi/config"
 	"goapi/routes"
+	"log"
 
 	"github.com/gin-gonic/gin"
+	"github.com/joho/godotenv"
 )
 
 func main() {
+
+	//Load .env file
+	if err := godotenv.Load(); err != nil {
+		log.Fatal("Error loading .env file")
+	}
 
 	//DB
 	config.ConnectDB()
 
 	r := gin.Default()
-
 	routes.SetupRouters(r)
-
 	r.Run(":8080")
 }
